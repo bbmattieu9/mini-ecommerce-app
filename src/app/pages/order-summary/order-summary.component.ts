2ed33915e50c4d841cb64b442bae12d996898edd
bbmattieu9 <bbmattieu9@gmail.com>
2025-02-06 16:04:19 +0100

    [Add] Implement order summary with computed

--- src/app/pages/order-summary/order-summary.component.ts
+++ src/app/pages/order-summary/order-summary.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {CartService} from '../../data-access/cart.service';
+import {PrimaryButtonComponent} from '../../components/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [],
+  imports: [
+    PrimaryButtonComponent
+  ],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss'
 })
 export class OrderSummaryComponent {
+
+  cartSrv = inject(CartService);
+  total = computed(() => {
+    let total = 0;
+    for (const item of this.cartSrv.cart()) {
+      total += item.price;
+    }
+    return total;
+  })
 
 }
